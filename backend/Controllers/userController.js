d62508e03ff4a17390c9c9ea6ef9811412d43fb7
vmarrerop <77476818+vmarrerop@users.noreply.github.com>
2023-10-06 02:50:40 -0500

    esd

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -5,64 +5,32 @@ const bcrypt = require("bcrypt");
 //==================> Create user <=======================
 const register = async (req, res) => {
   try {
-    let Body = req.body;
-    const { username, email, phone, password } = Body;
+    const { username, email, phone, password } = req.body;
 
-    if (!Body.username) {
-      return res.status(400).json("Please enter username");
-    }
-
-    //==================> Email validation <=======================
-    if (!Body.email) {
-      return res.status(400).json("Please enter email");
-    }
-    const Emailregx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let Email = Emailregx.test(Body.email);
-    if (!Email) {
-      return res.status(400).json("Please enter valid email.");
+    if (!username || !email || !phone || !password) {
+      return res.status(400).json("Por favor, completa todos los campos.");
     }
 
-    //<===================
-    const dublicateEmail = await userModel.findOne({ email: email });
-    if (dublicateEmail) {
-      return res.status(400).json(" Email Already Exists");
-    }
-    
-    //==================> Phone validation <=======================
-    if (!Body.phone) {
-      return res.status(400).json("Please enter phone number");
-    }
-    const Phoneregx = /^[0-9]{10}$/;
-    let Phone = Phoneregx.test(Body.phone);
-    if (!Phone) {
-      return res.status(400).json("Please enter valid Phone number.");
+    // Verifica si el correo electrónico ya existe en la base de datos
+    const existingUser = await userModel.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json("El correo electrónico ya está registrado.");
     }
 
-    //<===================
-    const dublicatePhone = await userModel.findOne({ phone });
-    if (dublicatePhone) {
-      return res.status(400).json(" Number Already Exists");
-    }
-
-    //==================> password validation <=======================
-    if (!Body.password) {
-      return res.status(400).json("Please enter password");
-    }
-    const Passregx =
-      /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&])[a-zA-Z0-9@#$%&]{8,}$/;
-    let Password = Passregx.test(Body.password);
-    if (!Password) {
-      return res
-        .status(400)
-        .json(
-          "Password must have atleast 1 uppercase\n, 1 lowercase, 1 special charecter\n 1 number and must consist atleast 8 charectors."
-        );
-    }
+    // Hashea la contraseña antes de guardarla en la base de datos
     const salt = await bcrypt.genSalt(10);
-    req.body.password = await bcrypt.hash(req.body.password, salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-    let savedData = await userModel.create(Body);
-    res.status(201).send({ data: savedData });
+    // Crea el nuevo usuario en la base de datos
+    const newUser = await userModel.create({
+      username,
+      email,
+      phone,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: "Registro exitoso.", user: newUser });
   } catch (error) {
     return res.status(500).json(error.message);
   }
@@ -70,45 +38,34 @@ const register = async (req, res) => {
 
 //==================> Login user <=======================
 
+// Cambia la función loginUser
 const loginUser = async function (req, res) {
   try {
-    let Body = req.body;
-    const { email, password } = Body;
-    
-    if (!email) {
-      return res.status(400).json("Please enter email address");
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json("Por favor, ingresa correo electrónico y contraseña.");
     }
-    
-    if (!password) {
-      return res.status(400).json("Please enter password");
+
+    // Verifica la existencia del usuario en la base de datos
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json("Correo electrónico o contraseña incorrectos.");
     }
-    
-    let getUser = await userModel.findOne({  email });
-    if (!getUser) return res.status(401).json("Email or Password is incorrect.");
-    
-    let matchPassword = await bcrypt.compare(password, getUser.password);
-    if (!matchPassword) return res.status(401).json("Email or Password is incorrect.");
-    
-    //token
-    
-    const token = jwt.sign(
-      {
-        userId: getUser._id.toString(),
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-      );
-      const { newPassword, ...other } = getUser
-      let User = getUser
-      
-      res.cookie("access_token", token, 
-      {
-        httpOnly: true,
-      }).status(200).json({User, token});
-      
-    } catch (error) {
-      return res.status(500).json(error.message);
+
+    // Compara las contraseñas
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json("Correo electrónico o contraseña incorrectos.");
     }
+
+    // Aquí puedes retornar la respuesta de éxito, o hacer lo que necesites.
+    res.status(200).json({ message: "Inicio de sesión exitoso." });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
 };
 
 
